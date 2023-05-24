877ce21e6014d460c804034343300e72817a84fc
Ahmed ElNahas <ahmed@circleci.com>
2023-05-24 12:26:51 -0400

    add chrome project

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,18 @@
 // @ts-check
 const path = require('path')
+const {devices} = require('@playwright/test')
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  * @type{import('@playwright/test').PlaywrightTestConfig}
  */
 const config = {
+  projects:[
+    {
+      name: "chrome",
+      use: {...devices['Desktop Chrome']}
+    }
+  ],
   webServer: {
     port: 9900,
     command: 'npm run start',
