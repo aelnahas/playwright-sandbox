039dc273a23aeef5c9712e3572eb62f6086e231b
Ahmed ElNahas <ahmed@circleci.com>
2023-05-24 11:42:02 -0400

    introduce a failing test

--- tests/show-battery-status.spec.js
+++ tests/show-battery-status.spec.js
@@ -20,7 +20,7 @@ test.beforeEach(async ({ page }) => {
 
 test('show battery status', async ({ page }) => {
   await page.goto('/');
-  await expect(page.locator('.battery-percentage')).toHaveText('90%');
+  await expect(page.locator('.battery-percentage')).toHaveText('failing test !!!!');
   await expect(page.locator('.battery-status')).toHaveText('Adapter');
   await expect(page.locator('.battery-fully')).toHaveText('00:30');
 })
